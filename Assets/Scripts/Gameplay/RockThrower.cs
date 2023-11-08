e0135fb9541fc0d058ed11496059499ea10d3186
BingoMango <103103876+Dange1TheMange1@users.noreply.github.com>
2023-11-08 10:26:26 +0100

    Her Jacob

--- Assets/Scripts/Gameplay/RockThrower.cs
+++ Assets/Scripts/Gameplay/RockThrower.cs
@@ -6,11 +6,10 @@ using UnityEngine;
 
 public class RockThrower : MonoBehaviour
 {
-    float speed = 4f;
-    float horizontalSpeed = 5f;
+    public float speed = 15f;
+    public float horizontalSpeed = 10f;
 
     float stunTimer = -1; //magic number lmfaoooo
-    float stunDirection = 0;
 
     Rigidbody rb;
     Animator an;
@@ -27,22 +26,19 @@ public class RockThrower : MonoBehaviour
     {
         if (stunTimer > 0)
         {
-            rb.velocity = new Vector3(stunDirection, 0, speed/2);
-            print(stunDirection);
+            rb.velocity = new Vector3(0, speed/2);
 
             stunTimer -= Time.deltaTime;
         }
         else
         {   
-            print(stunDirection);
             rb.velocity = new Vector3(Input.GetAxis("Horizontal") * horizontalSpeed, 0, speed);
         }
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
         print("hit");
-        stunTimer = 0.15f;
-        stunDirection = 15;
+        stunTimer = 1;
 
         //animation part does not work >:(
         an.enabled = true;
