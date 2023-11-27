ef3f8c81b5543a94766a01b4fea34701f9af4768
DiscoLucas <47451042+DiscoLucas@users.noreply.github.com>
2023-11-27 14:33:37 +0100

    Tried to add physics controller to ship

--- Assets/Scripts/Kinect/PlayerManager.cs
+++ Assets/Scripts/Kinect/PlayerManager.cs
@@ -9,22 +9,38 @@ using Unity.VisualScripting;
 public class PlayerManager : MonoBehaviour
 {
 
-    public Rigidbody rigidbody;
+    public Rigidbody rb;
     public float deadzone = 1;
+
+    [Header("speed variables")]
     public float movementSpeed;
+    public float longitudinalSpeed;
+    public float lift = 135f;
+    public float diveSpeedMulitplier = 2f;
     public float speedModifier;
+    [Tooltip("How quickly the ship responds to pitch and roll input")]
+    public float responsiveness = 15f;
+
+    private float roll;
+    private float pitch;
+
     public bool useKinect;
 
-    Controls controls;
-    
+    private float responseModifier // adjust responsiveness based on mass
+    {
+        get
+        {
+            return (rb.mass / 10f) * responsiveness;
+        }
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        controls = new Controls();
-        controls.Gameplay.Enable();
-
-        rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        //transform.position = waypoints[waypointIndex].transform.position;
+        
     }
 
     // Update is called once per frame
@@ -39,7 +55,23 @@ public class PlayerManager : MonoBehaviour
         {
             DebugMovement();
         }
+
+        // TODO: add resting force to make the ship return to upright when not moving
+        //float relativeVelocity = rigidbody.velocity.z;
+        //rigidbody.AddRelativeTorque(Vector3.right * relativeVelocity);
+
+        // Adjust lift force based on pitch angle
+        /*float pitchAngle = Mathf.Abs(transform.eulerAngles.z);
+        float pitchFactor = Mathf.Clamp01(pitchAngle / 90f); // may need to be adjusted
+        float  currentLift = lift * (1f - pitchFactor);
+        rb.AddForce(Vector3.up * currentLift, ForceMode.Acceleration);*/
+
+        rb.AddForce(transform.forward * longitudinalSpeed);
+        rb.AddForce(Vector3.up * rb.velocity.magnitude * lift);
+
     }
+
+  
 
     void KinectMovement()
     {
@@ -51,7 +83,7 @@ public class PlayerManager : MonoBehaviour
             float xAxis = transform.position.x;
             if (xAxis > deadzone || xAxis < -deadzone)
             {
-                rigidbody.AddForce(xAxis, 0, 0); // TODO: change add force to affect zAxis instead
+                rb.AddForce(xAxis, 0, 0); // TODO: change add force to affect zAxis instead
             }
 
         }
@@ -59,8 +91,14 @@ public class PlayerManager : MonoBehaviour
 
     void DebugMovement()
     {
-        rigidbody.AddForce(0, 0, -Input.GetAxis("Horizontal") * speedModifier);
-        rigidbody.MovePosition(transform.position + Vector3.forward * Time.deltaTime * movementSpeed);
+        rb.AddForce(0, 0, -Input.GetAxis("Horizontal") * responseModifier);
+        
+        //rb.AddTorque(transform.right * -Input.GetAxis("Horizontal") * responseModifier); //roll thing
+        
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.AddForce(Vector3.right * rb.velocity.magnitude * diveSpeedMulitplier, ForceMode.Impulse);
+    }
 }
